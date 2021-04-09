f4215b1152da7567525c12fb6a93d415060dd01a
Kutsenko Dmitry <koutsenko@gmail.com>
2021-04-09 14:21:17 +0300

    Removed unnecessary file close

--- scb/pray.py
+++ scb/pray.py
@@ -15,12 +15,8 @@ def pray(context: CallbackContext):
     script_path = path.realpath(path.join(getcwd(), path.dirname(__file__)))
     pray_path = path.join(script_path, 'pray.txt')
 
-    # Открытие файла через контекст
+    # Чтение из файла (будет закрыт автоматически) и вывод в чат
     with open(pray_path, 'r', encoding='utf-8') as pray_file:
-        # Сохранение молитвы в переменную
         pray_text = pray_file.read()
-        pray_file.close()
-
-        # Отправка молитвы в целевой чат
         channel_id = context.bot_data['config']['CHAT']
         context.bot.send_message(chat_id=channel_id, text=pray_text)
